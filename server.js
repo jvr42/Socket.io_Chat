d4627409084b45247e0aa9e0750e3ea271328fe7
Jesus Villarroel <jvr42@hotmail.com>
2015-07-28 01:48:11 -0300

    mensaje sencillo

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+server.listen(app.get('port') ,app.get('ip'), function () {
+    console.log("Server listening at %s:%d ", app.get('ip'),app.get('port'));
+});
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
