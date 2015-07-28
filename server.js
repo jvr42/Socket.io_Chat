e8c4e85b58f97be58f13c459e98ceaf2f91cf305
Jesus Villarroel <jvr42@hotmail.com>
2015-07-28 01:37:57 -0300

    mensaje sencillo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
